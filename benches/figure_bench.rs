1d28d3bc72ba967a824eaf06140ee8111bc390f3
LAURENT Louis <louis_laurent@aol.fr>
2021-04-22 17:23:48 +0200

    add bench

---
+++ benches/figure_bench.rs
@@ -0,0 +1,63 @@
+use rand::prelude::*;
+use criterion::{ criterion_group, criterion_main, Criterion, BenchmarkId };
+use dod_color_bench::figure::{ Shape, Shapes, ShapeType, count_number_rect_oob, count_number_rect_dod };
+
+fn generate_shape_list(number: u8) -> Vec<Shape> {
+    let mut rng = rand::thread_rng();
+    
+    return (0..number).into_iter().map(|_| {
+        return Shape {
+            left_conner_x: rng.gen_range(1..100),
+            left_conner_y: rng.gen_range(1..100),
+            width: rng.gen_range(1..100),
+            height: rng.gen_range(1..100),
+            color: "#ff0000".to_string(),
+            shape_type: random_shape_type(rng.gen_range(0..2))
+        };
+    }).collect::<Vec<Shape>>();
+}
+
+fn random_shape_type(number: u8) -> ShapeType {
+    return match number {
+        0 => ShapeType::RECT,
+        _ => ShapeType::CIRCLE
+    };
+}
+
+fn generate_shapes(number: u8) -> Shapes {
+    let mut rng = rand::thread_rng();
+
+    return (0..number).into_iter().fold(Shapes::new(), |mut acc, _| {
+            acc.left_conners_x.push(rng.gen_range(1..100));
+            acc.left_conners_y.push(rng.gen_range(1..100));
+            acc.widths.push(rng.gen_range(1..100));
+            acc.heights.push(rng.gen_range(1..100));
+            acc.colors.push("#ff0000".to_string());
+            acc.shapes_type.push(random_shape_type(rng.gen_range(0..2)));
+
+            return acc;
+        });
+}
+
+fn run_oob_dod_bench(c: &mut Criterion) {
+    let mut group = c.benchmark_group("oob_dod");
+    let bench_range: [u8; 6] = [10, 20, 50, 100, 150, 200];
+    
+    for number_element  in bench_range.iter() {
+        group.bench_with_input(
+        BenchmarkId::new("oob", number_element), 
+        &generate_shape_list(*number_element), 
+        |b, number_element| b.iter(|| count_number_rect_oob(number_element))
+        );
+        group.bench_with_input(
+        BenchmarkId::new("dod", number_element),
+        &generate_shapes(*number_element).shapes_type, 
+        |b, number_element| b.iter(|| count_number_rect_dod(number_element))
+        );
+    }
+
+    group.finish();
+}
+
+criterion_group!(benches, run_oob_dod_bench);
+criterion_main!(benches);
---
+++ src/figure.rs
@@ -0,0 +1,57 @@
+#[derive(Debug, Clone, PartialEq)]
+pub enum ShapeType {
+    RECT,
+    CIRCLE
+}
+#[derive(Debug, Clone, PartialEq)]
+pub struct Shape {
+    pub left_conner_x: i32,
+    pub left_conner_y: i32,
+    pub width: u16,
+    pub height: u16,
+    pub color: String,
+    pub shape_type: ShapeType
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Shapes {
+    pub left_conners_x: Vec<i32>,
+    pub left_conners_y: Vec<i32>,
+    pub widths: Vec<u16>,
+    pub heights: Vec<u16>,
+    pub colors: Vec<String>,
+    pub shapes_type: Vec<ShapeType>
+}
+
+impl Shapes {
+    pub fn new() -> Self {
+        return Shapes {
+            left_conners_x: Vec::new(),
+            left_conners_y: Vec::new(),
+            widths: Vec::new(),
+            heights: Vec::new(),
+            colors: Vec::new(),
+            shapes_type: Vec::new()
+        }
+    }
+}
+
+pub fn count_number_rect_oob(shapes: &[Shape]) -> (u16, u16) {
+    return shapes.iter()
+        .fold((0, 0), |acc, shape| {
+            return match shape.shape_type {
+                ShapeType::RECT => (acc.0 + 1, acc.1),
+                ShapeType::CIRCLE => (acc.0, acc.1 + 1)
+            };
+        });
+}
+
+pub fn count_number_rect_dod(shapes: &[ShapeType]) -> (u16, u16) {
+    return shapes.iter()
+    .fold((0, 0), |acc, shape_type| {
+        return match shape_type {
+            ShapeType::RECT => (acc.0 + 1, acc.1),
+            ShapeType::CIRCLE => (acc.0, acc.1 + 1)
+        };
+    });
+}
